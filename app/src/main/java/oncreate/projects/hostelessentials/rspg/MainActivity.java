d718873e010b5d988a01e0de065ef88a5b26ecb5
Irfan_S <irfansa1@ymail.com>
2019-03-21 15:57:54 +0530

    Base Commit

---
+++ app/src/main/java/oncreate/projects/hostelessentials/rspg/MainActivity.java
@@ -0,0 +1,14 @@
+package oncreate.projects.hostelessentials.rspg;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
